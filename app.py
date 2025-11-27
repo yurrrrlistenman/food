45d674742aef016e824612e85e48d27500296125
yurrrrlistenman <nodir.khaltaev@gmail.com>
2025-11-27 15:11:38 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dish Recommender", page_icon="🍳")
+st.title("🍳 Ingredient-to-Dish Recommender")
+st.write("If you see this, your Streamlit app.py is working!")
